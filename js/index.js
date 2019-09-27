832c2bc6f054e71fb0055004a3b944468f63b4c6
Rodrigo Tadeu <rodrigotfonsecas@outlook.com>
2019-09-26 22:16:42 -0300

    adicionando novas linhas ao código

--- js/index.js
+++ js/index.js
@@ -49,6 +49,33 @@ $(document).ready(() => {
   })
 })
 
+function mascaraDeTelefone(telefone){
+  const textoAtual = telefone.value; 
+  const isCelular = textoAtual.length===9; 
+
+  let textoAjustado; 
+
+  if(isCelular){
+    const parte1 = textoAtual.slice(0,5);
+    const parte2 = textoAtual.slice(5,9);
+    textoAjustado = `${parte1}-${parte2}`
+  }else{
+    const parte1 = textoAtual.slice(0,4);
+    const parte2 = textoAtual.slice(4,8);
+    textoAjustado = `${parte1}-${parte2}`
+  }
+
+  telefone.value = textoAjustado;
+
+}
+
+function tirarHifen(telefone){
+  const textoAtual = telefone.value;
+  const textoAjustado = textoAtual.replace(/\-/g, '');
+
+  telefone.value = textoAjustado;
+}
+
 // $('.slick--slider-for').slick({
 //     slidesToShow: 1,
 //     slidesToScroll: 1,
